d84525a7e1cdfe7ea3715d90b729be83bcc74a11
Hermogenes Batista da Silva Filho <exemogenes@gmail.com>
2015-01-24 15:41:55 -0300

    Teste

---
+++ app/src/main/java/br/com/moge/academico/LoginActivity.java
@@ -0,0 +1,38 @@
+package br.com.moge.academico;
+
+import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+
+public class LoginActivity extends ActionBarActivity {
+    private EditText edtLogin;
+    private EditText edtPassword;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        edtLogin = (EditText) findViewById(R.id.email);
+        edtPassword = (EditText) findViewById(R.id.senha);
+
+    }
+
+    public void login(View v){
+
+        if(edtLogin.getText().toString().equals("admin@cd.com.br") && edtPassword.getText().toString().equals("123")){
+            Toast.makeText(this, "Login efetuado com sucesso!", Toast.LENGTH_LONG).show();
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+
+        }else{
+            edtPassword.setError("Senha e/ou e-mail incorreto(s)!");
+        }
+    }
+
+
+}

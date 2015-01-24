74f52ddf518655b26d08e346f0f9c867b8fbc39d
Hermogenes Batista da Silva Filho <exemogenes@gmail.com>
2015-01-24 16:45:50 -0300

    Implementando o ListAdapter

--- app/src/main/java/br/com/moge/academico/ListCursoAdapter.java
+++ app/src/main/java/br/com/moge/academico/ListCursoAdapter.java
@@ -1,14 +1,29 @@
 package br.com.moge.academico;
 
+import android.app.Activity;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+
+import java.util.List;
 
 /**
  * Created by cd-107 on 24/01/2015.
  */
 public class ListCursoAdapter extends BaseAdapter{
 
+    private Activity activity;
+    private List<Curso> cursos;
+
+    public static ListCursoAdapter getInstance(Activity activity, List<Curso> cursos){
+        ListCursoAdapter adapter = new ListCursoAdapter();
+        adapter.activity = activity;
+        adapter.cursos = cursos;
+
+        return adapter;
+    }
+
+    private ListCursoAdapter(){}
 
     @Override
     public int getCount() {

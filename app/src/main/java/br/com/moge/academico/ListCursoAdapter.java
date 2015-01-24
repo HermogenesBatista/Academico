78be9d4ae0ef3d71ef1e937f5926fb6cafa12130
Hermogenes Batista da Silva Filho <exemogenes@gmail.com>
2015-01-24 16:36:04 -0300

    Mostrar Lista

---
+++ app/src/main/java/br/com/moge/academico/ListCursoAdapter.java
@@ -0,0 +1,32 @@
+package br.com.moge.academico;
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+/**
+ * Created by cd-107 on 24/01/2015.
+ */
+public class ListCursoAdapter extends BaseAdapter{
+
+
+    @Override
+    public int getCount() {
+        return 0;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        return null;
+    }
+}
